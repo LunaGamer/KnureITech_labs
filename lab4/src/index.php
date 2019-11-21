8a3e969e62f9b02a5d7dec73c5e0d6d8b2710913
LunaGamer <azazel129813@mail.ru>
2019-11-21 19:43:56 +0200

    all labs

--- lab4/src/index.php
+++ lab4/src/index.php
@@ -9,7 +9,7 @@ session_start();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>TaranLab4</title>
 </head>
 <body>
 <form class="form-name">
